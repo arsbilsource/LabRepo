61d5b0606f1a2acdc1234c6711fb68f99e017bb2
arsbilsource <aibutylkin@miem.hse.ru>
2022-10-22 00:50:04 +0300

    Created a bad feature

--- 3_Translator/Translator.cpp
+++ 3_Translator/Translator.cpp
@@ -1,1 +1,25 @@
 #include "Translator.h"
+
+    void Add(string_view source, string_view target){
+        forward.emplace(std::make_pair(source, target));
+        auto it = forward.find(std::string(source));
+        auto it2 = backward.find(target);
+        if(it2 != backward.end()) {
+            backward.erase(it2);
+        }
+        backward.emplace(it->second, it->first);
+    }
+
+    string_view TranslateForward(string_view source) const{
+        auto it = forward.find(std::string(source));
+        if(it != forward.end()){
+            return it->second;
+        } else return "";
+    }
+
+    string_view TranslateBackward(string_view target) const{
+        auto it = backward.find(target);
+        if(it != backward.end()){
+            return it->second;
+        } else return "";
+    }
--- 3_Translator/Translator.h
+++ 3_Translator/Translator.h
@@ -13,29 +13,9 @@ public:
 
     Translator() = default;
 
-    void Add(string_view source, string_view target){
-        forward.emplace(std::make_pair(source, target));
-        auto it = forward.find(std::string(source));
-        auto it2 = backward.find(target);
-        if(it2 != backward.end()) {
-            backward.erase(it2);
-        }
-        backward.emplace(it->second, it->first);
-    }
-
-    string_view TranslateForward(string_view source) const{
-        auto it = forward.find(std::string(source));
-        if(it != forward.end()){
-            return it->second;
-        } else return "";
-    }
-
-    string_view TranslateBackward(string_view target) const{
-        auto it = backward.find(target);
-        if(it != backward.end()){
-            return it->second;
-        } else return "";
-    }
+    void Add(string_view source, string_view target);
+    string_view TranslateForward(string_view source) const;
+    string_view TranslateBackward(string_view target) const;
 
 
 private:
